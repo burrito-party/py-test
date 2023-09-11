0d5f5c4dd33cef1b0c7add0e7e54a3b6d0ca6c34
Dan Shanahan <leftrightleft@github.com>
2023-09-11 09:53:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import re
+
 print("Hello")
+
+def _is_inverse_proxy_host(host):
+    if host:
+        return re.match(r'\S+.googleusercontent.com/{0,1}$', host)
+    if re.match(r'\w+', host):
+        warnings.warn(
+            'The received host is %s, please include the full endpoint address '
+            '(with ".(pipelines/notebooks).googleusercontent.com")' % host)
+    return False
+
+_is_inverse_proxy_host("123")
